671ed0dd5aa8ed532068cfb40d379d25488af5fd
DChambersSTFC <dylan.chambers@stfc.ac.uk>
2017-06-02 11:07:51 +0100

    Added new test and constants

---
+++ SimpleKafkaConsumer/src/EventMessage/EventMessagePOJOTest.java
@@ -0,0 +1,22 @@
+package eventmessage;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ISIS,STFC on 02/06/2017.
+ * Unit tests for EventMessagePOJO
+ */
+public class EventMessagePOJOTest {
+    private static final int DEFAULT_MESSAGE_ID = 0;
+    private static final int DEFAULT_PULSE_TIME = 0;
+
+
+    /**
+     * Testing of setting MessageID
+     * @throws Exception Generic Exception
+     */
+    @org.junit.Test
+    public void setMessageId() throws Exception {
+    }
+
+}
